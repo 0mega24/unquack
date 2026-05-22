931e28dfecffafd1c124b8cfba933154bb3121d3
Sean Lidy <sean@lidy.me>
2026-05-22 13:17:03 -0400

    chore: mobile fixes and hosting fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import { compression } from "vite-plugin-compression2";
 
 export default defineConfig({
   appType: "spa",
+  css: {
+    transformer: "lightningcss",
+  },
+  build: {
+    cssMinify: "lightningcss",
+    modulePreload: { polyfill: false },
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          bangs: ["./src/bang.ts"],
+        },
+      },
+    },
+  },
   plugins: [
+    compression({ algorithm: "gzip", exclude: /\.(html|webmanifest)$/ }),
     VitePWA({
       registerType: "autoUpdate",
     }),
